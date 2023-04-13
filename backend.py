23b27c82f423250113221933c8a2e221625e68d2
Isaac Perkins <iperkins@uoregon.edu>
2023-04-12 18:21:05 -0700

    Added basic templates and setup index page

--- backend.py
+++ backend.py
@@ -1,10 +1,12 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
-@app.route("/time")
-def time_series():
-    return {"timeseries": ["data01", "data02", "data03"]}
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
