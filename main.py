8572998c45f29645fce04828e6c4ea69a2bb5875
Geoffrey Brendel <geoff@pektel.com>
2023-04-14 21:09:25 -0700

    Made upload_data upload to a potential mongoDB database. Also added download_data to display current files in the database with a link to download the file.

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, url_for
+from flask_pymongo import PyMongo
 
 
 app = Flask(__name__)
+
+# MONGO CONNECTION:
+app.config['MONGO_URI'] = 'mongodb://localhost:27017/myDatabase'
+mongo = PyMongo(app)
 
 
 @app.route("/home")
@@ -14,14 +19,34 @@ def home():
 def upload_data():
     return render_template("upload_data.html")
 
-#Upload route to ./templates/uploads so when we upload a file it gets stored in uploads.
+#----------------------------------Start TO MONGO routes--------------------
+#Upload route to ./templates/uploads so when we upload a file it gets stored in uploads. Records username with it.
 @app.route('/upload', methods = ['POST'])  
 def upload():
-    file = request.files['file']
+    if 'file' in request.files:
+        file = request.files['file']
+        mongo.save_file(file.filename, file)
+        mongo.db.users.insert({'username': request.form.get('username'), 'file' : file.filename})
+    return redirect('/')    
+    
 
-    file.save(f'./templates/uploads/{file.filename}')
+#Way to retrieve data from database
+@app.route('/download_data/<filename>')
+def file(filename):
+    return mongo.send_file(filename)
 
-    return redirect('/')
+#Displays current data in the fs.files within database
+#fs.files can be swapped with whatever the collection is on the MongoDB database
+@app.route("/download_data", methods = ['GET'])
+def filenames():
+    try:
+        filenames = mongo.db.fs.files.find({})
+        return render_template('download_data.html', filenames = filenames)
+    except Exception:
+        return redirect('/')
+
+#-------------------------END OF MONGO----------------------------------#
+    
 
 @app.route("/download_data")
 def download_data():
